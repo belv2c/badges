970fee6dc6f6158cd10e782e446e0e66dafc6534
Brooke Lewis <belv2c@gmail.com>
2017-10-07 12:04:35 -0500

    badges print

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const buildDomString = (treehouseData) => {
+	let domString = '';
+	treehouseData.forEach((badges) => {
+		domString += `<div>`;
+		domString += 	`<img src=${badges.icon_url}>`;
+		domString += `</div>`;
+	});
+	printDomString(domString);
+};
+
+const printDomString = (string) => {
+	$('#badgeContainer').html(string);
+};
+
+module.exports = {buildDomString};
